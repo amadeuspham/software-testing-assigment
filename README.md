5a1ea5837014a5b6835a10d30a2c582259948ec0
Harry Pham <scramblegf@gmail.com>
2020-10-21 15:54:48 +0300

    First test written, readme updates

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Software Testing assignment phase 2 
 [![Coverage Status](https://coveralls.io/repos/github/amadeuspham/software-testing-assigment/badge.svg?branch=main)](https://coveralls.io/github/amadeuspham/software-testing-assigment?branch=main)
+
+## Installation
+1. First, make sure you have [Node](https://nodejs.org/en) installed. This project can run on Node 13 or 14 (14 is recommended). Run `node -v` in Terminal to check your version.
+
+2. Install necessary dependencies
+```
+npm install
+```
+
+## Usage
+3. Run tests (all tests are inside /src/__tests__)
+```
+npm test
+```
+3. Run linter
+```
+npm run lint
+```
---
+++ src/__tests__/unit/add.js
@@ -0,0 +1,24 @@
+import add from '../add.js'
+
+describe('add should sum two numbers', () => {
+  it('should sum two positive integer', () => {
+    const sum = add(3, 4)
+    expect(sum).toBe(7)
+  })
+  it('should sum two negative integer', () => {
+    const sum = add(-3, -2)
+    expect(sum).toBe(-5)
+  })
+  it('should sum both negative and positive integers', () => {
+    const sum = add(-3, 4)
+    expect(sum).toBe(1)
+  })
+  it('sum of a number and zero is the number itself', () => {
+    const sum = add(3, 0)
+    expect(sum).toBe(3)
+  })
+  it('should sum decimals', () => {
+    const sum = add(3.5, -1.2)
+    expect(sum).toBe(2.3)
+  })
+})
