7088558ec86564dc5b1b2c522d5bfffab12f3677
Harry Pham <scramblegf@gmail.com>
2020-11-14 18:21:15 +0200

    More coverage for endsWith.js

--- src/__tests__/unit/endsWith.js
+++ src/__tests__/unit/endsWith.js
@@ -10,4 +10,13 @@ describe('endsWith checks if the string ends with the target string', () => {
   it('should return true that Charles ends with a at index 3', () => {
     expect(endsWith('Diana', 'a', 3)).toBe(true)
   })
+  it('should automatically correct the position index to 0 if the given value is negative', () => {
+    expect(endsWith('Diana', 'a', -5)).toBe(false)
+    expect(endsWith('Diana', 'D', -5)).toBe(false)
+    expect(endsWith('Diana', '', -5)).toBe(true)
+  })
+  it('should automatically correct the position index to the string length if the given value is larger than the length', () => {
+    expect(endsWith('Diana', 'a', 50)).toBe(true)
+    expect(endsWith('Diana', 'D', 50)).toBe(false)
+  })
 })
