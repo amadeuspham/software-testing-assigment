deac62d213aed630db6c22e909c71f64e31b5c64
Harry Pham <scramblegf@gmail.com>
2020-12-02 01:41:29 +0200

    Test for reduce.js

--- src/__tests__/unit/reduce.js
+++ src/__tests__/unit/reduce.js
@@ -1,1 +1,7 @@
+import reduce from '../../reduce'
 
+describe('reduce should accumulate elements in an array to an optional accumulator, with a given iteratee function applied on each element', () => {
+  it('should work as a typical summation operation', () => {
+    expect(reduce([1, 2], (sum, n) => sum + n, 0)).toBe(3)
+  })
+})
