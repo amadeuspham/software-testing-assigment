6df4fef44bfa31190816d1def42465b204f23421
Harry Pham <scramblegf@gmail.com>
2020-12-05 02:56:23 +0200

    More test cases

--- src/__tests__/unit/endsWith.js
+++ src/__tests__/unit/endsWith.js
@@ -4,6 +4,9 @@ describe('endsWith checks if the string ends with the target string', () => {
   it('should return true that Diana ends with a', () => {
     expect(endsWith('Diana', 'a')).toBe(true)
   })
+  it('should return true that Diana ends with na', () => {
+    expect(endsWith('Diana', 'na')).toBe(true)
+  })
   it('should return false that Charles ends with a', () => {
     expect(endsWith('Charles', 'a')).toBe(false)
   })
